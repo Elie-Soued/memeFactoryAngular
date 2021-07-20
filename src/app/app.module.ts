05488816c8bf4cf730f839c4a8270dc631718ae5
Elie Soued <eliegebransoued@gmail.com>
2021-07-20 18:21:00 +0200

    Modules and Components added automatically to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { TextinputComponent } from './components/textinput/textinput.component';
 import { ImageHolderComponent } from './components/image-holder/image-holder.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    TextinputComponent,
-    ImageHolderComponent,
+  declarations: [AppComponent, TextinputComponent, ImageHolderComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
