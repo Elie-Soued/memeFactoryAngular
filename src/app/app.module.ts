648ef58c3eeb9bdaaa371551a1a1f7f7ffdb392f
Elie Soued <eliegebransoued@gmail.com>
2021-09-06 19:19:45 +0200

    remove duplicate import of the dragDrop module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
 import { ImageHolderComponent } from './components/image-holder/image-holder.component';
