87c8337c6195cc0f9415c1ec275fe05a4ed8b3d0
Elie Soued <eliegebransoued@gmail.com>
2021-09-08 21:11:29 +0200

    remove dead code

--- src/app/components/image-holder/image-holder.component.ts
+++ src/app/components/image-holder/image-holder.component.ts
@@ -6,55 +6,16 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./image-holder.component.css'],
 })
 export class ImageHolderComponent implements OnInit {
-  //Image related properties
   index: number = 0;
   @Input() picture?: string;
-  @Input() textBox?: string;
-
-  //Input and TextBox
-  upperTextBoxColor: string = 'black';
-  lowerTextBoxColor: string = 'black';
-  upperTextBox?: string;
-  lowerTextBox?: string;
-  fontSizeUpper: number = 15;
-  fontSizeLower: number = 15;
+  @Input() upperTextBox?: string;
+  @Input() lowerTextBox?: string;
+  @Input() upperTextBoxColor: string = 'black';
+  @Input() lowerTextBoxColor: string = 'black';
+  @Input() fontSizeUpper: number = 15;
+  @Input() fontSizeLower: number = 15;
 
   constructor() {}
 
   ngOnInit(): void {}
-  setUpperTextBox(value: string) {
-    this.upperTextBox = value;
-  }
-
-  setLowerTextBox(value: string) {
-    this.lowerTextBox = value;
-  }
-
-  //Set the color for the text-box
-
-  setUpperTextBoxColor(value: string) {
-    this.upperTextBoxColor = value;
-  }
-
-  setLowerTextBoxColor(value: string) {
-    this.lowerTextBoxColor = value;
-  }
-
-  //Set the size for the text-box
-
-  increaseTextBoxUpper() {
-    this.fontSizeUpper = this.fontSizeUpper + 1;
-  }
-
-  decreaseTextBoxUpper() {
-    this.fontSizeUpper = this.fontSizeUpper - 1;
-  }
-
-  increaseTextBoxLower() {
-    this.fontSizeLower = this.fontSizeLower + 1;
-  }
-
-  decreaseTextBoxLower() {
-    this.fontSizeLower = this.fontSizeLower - 1;
-  }
 }
