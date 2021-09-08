893f3340db6465148b2e4e01416d4147897ac0e8
Elie Soued <eliegebransoued@gmail.com>
2021-09-08 20:00:08 +0200

    add inputs to image-holder component + remove unnecessary properties

--- src/app/components/image-holder/image-holder.component.ts
+++ src/app/components/image-holder/image-holder.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ImagesService } from '../../images.service';
-import { FormControl } from '@angular/forms';
-import { Image } from '../../image';
-import { SanitizePipe } from '../../sanitize.pipe';
-import * as htmlToImage from 'html-to-image';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-image-holder',
@@ -12,11 +7,9 @@ import * as htmlToImage from 'html-to-image';
 })
 export class ImageHolderComponent implements OnInit {
   //Image related properties
-  images?: Image[];
   index: number = 0;
-  centralPicture?: string;
-  uploadedPicture?: string;
-  generatedImage?: string;
+  @Input() picture?: string;
+  @Input() textBox?: string;
 
   //Input and TextBox
   upperTextBoxColor: string = 'black';
@@ -26,85 +19,9 @@ export class ImageHolderComponent implements OnInit {
   fontSizeUpper: number = 15;
   fontSizeLower: number = 15;
 
-  fileInput = new FormControl('');
+  constructor() {}
 
-  constructor(
-    private imageService: ImagesService,
-    private sanitize: SanitizePipe
-  ) {}
-
-  ngOnInit(): void {
-    this.getImages();
-  }
-
-  //Images related methods
-
-  getImages(): void {
-    this.imageService
-      .getImages()
-      .subscribe((object) => this.setImages(object.data.memes));
-  }
-
-  setImages(data: Image[]) {
-    this.images = data;
-    this.centralPicture = this.images[this.index].url;
-  }
-
-  goNext = () => {
-    if (this.images) {
-      if (this.index === this.images.length - 1) {
-        this.index = 0;
-        this.centralPicture = this.images[0].url;
-      } else {
-        this.centralPicture = this.images[++this.index].url;
-      }
-    }
-  };
-
-  goBack = () => {
-    if (this.images) {
-      if (this.index === 0) {
-        this.index = this.images.length - 1;
-        this.centralPicture = this.images[this.images.length - 1].url;
-      } else {
-        this.centralPicture = this.images[--this.index].url;
-      }
-    }
-  };
-
-  goRandom = () => {
-    if (this.images) {
-      let randomNumber = Math.floor(Math.random() * 100);
-      this.centralPicture = this.images[randomNumber].url;
-    }
-  };
-
-  onFileSelected(event: any) {
-    const uploaded = URL.createObjectURL(event.target.files[0]);
-    this.centralPicture = this.sanitize.transform(uploaded);
-  }
-
-  generate = (dom: HTMLElement) => {
-    htmlToImage
-      .toPng(dom)
-      .then((dataUrl) => {
-        this.generatedImage = dataUrl;
-      })
-      .catch(function (error) {
-        console.error('oops, something went wrong!', error);
-      });
-  };
-
-  reset = () => {
-    this.generatedImage = '';
-    this.upperTextBox = '';
-    this.lowerTextBox = '';
-  };
-
-  // Input  and TextBox related methods
-
-  //Set the Text for the text-box
-
+  ngOnInit(): void {}
   setUpperTextBox(value: string) {
     this.upperTextBox = value;
   }
